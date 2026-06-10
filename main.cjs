a4cc98f3d3800c8dbc680f267037c2e1d6b1adc7
ELADIO TORTOLERO <etortoler22@ujap.edu.ve>
2026-06-10 19:32:02 -0400

    cambios

---
+++ main.cjs
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.loadURL('http://localhost:5173');
+  } else {
+    mainWindow.loadFile(path.join(__dirname, 'dist/index.html'));
+  }
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
--- src/utils/ergonomics.js
+++ src/utils/ergonomics.js
@@ -134,13 +134,8 @@ export const evaluateRiskREBA = (angles, calibration = {}, options = {}) => {
     const baselineShrugRatio = calibration.shrug_ratio || 0;
     const currentShrugRatio = angles.shrug_ratio || 0;
     let isShrugging = false;
-<<<<<<< HEAD
     // If the trunk/neck ratio increases by 8%, it means shoulders are closer to ears
     if (baselineShrugRatio > 0 && currentShrugRatio > baselineShrugRatio * 1.08) {
-=======
-    // If the trunk/neck ratio increases by 15%, it means shoulders are closer to ears
-    if (baselineShrugRatio > 0 && currentShrugRatio > baselineShrugRatio * 1.15) {
->>>>>>> origin/main
         isShrugging = true;
     }
 
